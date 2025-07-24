061ecae696816994531507e46d1125a1d9123409
Naveen Prabodha <naveenprabodha3@gmail.com>
2025-07-25 03:39:15 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,20 +1,36 @@
+/*
+===============================================
+-- Script to create 'DataWarehouse' database and
+-- three schemas: bronze, silver, and gold.
+-- Schemas are used to organize data in layers.
+=============================================== 
+*/
+
+-- Switch to the 'master' database
 USE master;
 
+-- Create 'DataWarehouse' database if it doesn't already exist
 IF NOT EXISTS (
     SELECT name FROM sys.databases WHERE name = N'DataWarehouse'
 )
 BEGIN
-    CREATE DATABASE DataWarehouse;
+    CREATE DATABASE DataWarehouse;  -- Create the database
 END;
 
-GO
+GO  -- End of first batch
 
+-- Switch context to the newly created or existing DataWarehouse database
 USE DataWarehouse;
-GO
+GO  -- End of second batch
 
+-- Create schema 'bronze' for raw or ingested data
 CREATE SCHEMA bronze;
-GO
+GO  -- End of third batch
+
+-- Create schema 'silver' for cleansed and transformed data
 CREATE SCHEMA silver;
-GO
+GO  -- End of fourth batch
+
+-- Create schema 'gold' for curated, analytics-ready data
 CREATE SCHEMA gold;
-GO
+GO  -- End of fifth batch
